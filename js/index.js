9ba32b456d68ca690044de2fbb61e6adb445c6fe
Justin <info@justinlmartin.com>
2022-06-29 22:09:18 -0700

    Adding basic lambda call

---
+++ js/index.js
@@ -0,0 +1,18 @@
+async function callLambda() {
+    const url = 'https://fntikl9sah.execute-api.us-east-1.amazonaws.com/prod';
+    const response = await fetch(url, {
+        method: 'POST',
+        body: {
+            type: "GET_TEAM_NAMES"
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const myJson = await response.json();
+    console.log(myJson)
+}
+
+async function getTeamNames() {
+    await callLambda()
+}
