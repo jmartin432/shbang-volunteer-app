fce107af6b40967aa0485dbc02acb6bf537002b1
Justin <info@justinlmartin.com>
2022-08-15 12:24:36 -0700

    Cors fixed, team selector is populating and clearing

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-async function callLambda(options) {
+async function callLambda(type, options) {
+    console.log('calling lambda')
     const url = 'https://fntikl9sah.execute-api.us-east-1.amazonaws.com/prod/{proxy}';
     const response = await fetch(url, {
         method: 'POST',
         body: {
-            type: 'GET_TEAM_NAMES',
+            type: type,
             options: options
         },
         headers: {
@@ -13,8 +14,59 @@ async function callLambda(options) {
     return await response.json();
 }
 
-function populateTeamsSelect(names) {
+function populateTeamsSelect(teams) {
+    console.log('populating team select')
     let selector = document.getElementById('team-select')
+    teams.records.sort((a, b) => a.fields.Name.localeCompare(b.fields.Name))
+    for (let i = 0; i < teams.records.length; i++) {
+        let team = teams.records[i].fields.Name
+        let option = document.createElement('option')
+        let optionText = document.createTextNode(team);
+        option.appendChild(optionText)
+        option.setAttribute('id', 'name-select-option'.concat(i.toString()))
+        option.setAttribute('value', team)
+        option.setAttribute('innerHTML', team)
+        selector.appendChild(option)
+    }
+}
+
+function clearTeams() {
+    console.log('clearing all')
+    let selector = document.getElementById('team-select')
+    while (selector.children[1]) {
+        selector.removeChild(selector.children[1]);
+    }
+    selector.classList.add('hidden');
+    selector.value="";
+}
+
+function showTeamSelect() {
+    console.log('showing teams')
+    let selector = document.getElementById('team-select')
+    selector.classList.remove('hidden');
+}
+
+async function getTeamNames() {
+    clearTeams();
+    clearVolNames();
+    let teamNames = await callLambda('GET_TEAM_NAMES',{});
+    populateTeamsSelect(teamNames);
+    showTeamSelect();
+}
+
+function clearVolNames() {
+    console.log('clearing vol names')
+    let selector = document.getElementById('name-select')
+    while (selector.children[1]) {
+        selector.removeChild(selector.children[1]);
+    }
+    selector.classList.add('hidden');
+    selector.value="";
+}
+
+function populateNamesSelect(names) {
+    console.log('populating name select')
+    let selector = document.getElementById('name-select')
     names.records.sort((a, b) => a.fields.Name.localeCompare(b.fields.Name))
     for (let i = 0; i < names.records.length; i++) {
         let name = names.records[i].fields.Name
@@ -28,29 +80,17 @@ function populateTeamsSelect(names) {
     }
 }
 
-function clearAll() {
-    console.log('clearing all')
-    let label = document.getElementById('team-select-label')
-    let selector = document.getElementById('team-select')
-    while (selector.firstChild) {
-        selector.removeChild(selector.firstChild);
-    }
-    label.classList.add('hidden');
-    selector.classList.add('hidden');
-}
-
-function showTeamSelect() {
-    console.log('showing teams')
-    let label = document.getElementById('team-select-label')
-    let selector = document.getElementById('team-select')
-    label.classList.remove('hidden');
+function showNameSelect() {
+    console.log('showing names')
+    let selector = document.getElementById('name-select')
     selector.classList.remove('hidden');
 }
 
-async function getTeamNames() {
-    clearAll();
-    let teamNames = await callLambda({});
-    populateTeamsSelect(teamNames);
-    showTeamSelect();
+async function getVolNames(team) {
+    console.log('getting vol names')
+    clearVolNames();
+    let volNames = await callLambda('GET_VOL_NAMES',{team: team});
+    populateNamesSelect(volNames);
+    showNameSelect();
 }
 
